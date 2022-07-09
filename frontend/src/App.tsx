2c1c24332c6ad88be6974ad0e3abeb2678d30b32
Fabian Thorand <f.thorand@gmail.com>
2022-07-09 16:47:08 +0200

    Replace template assets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,68 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/esm/Alert';
+import Spinner from 'react-bootstrap/esm/Spinner';
 
 function App() {
-  useEffect(() => {
-    console.log(logo);
-  });
   return (
     <div className="App">
       <header>
         <h1>AutoRec</h1>
-        <Button>Hello World</Button>
       </header>
+      <SongList/>
     </div>
   );
+}
+
+function SongList() {
+  const [songsLoading, setSongsLoading] = useState(true);
+  const [songs, setSongs] = useState([]);
+  const [error, setError] = useState((null as null | string));
+
+  const fetchSongs = async() => {
+    try {
+      const response = await fetch("http://localhost:8000/songs");
+      const data = await response.json();
+      setSongs(data);
+      setError(null);
+    } catch(e) {
+      if(e instanceof Error) {
+        setError(e.message);
+      } else {
+        setError("Unknown error");
+      }
+    }
+    setSongsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+
+  if(songsLoading) {
+    return (
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    );
+  } else {
+    return (
+      <div>
+        {
+          error
+            ? (            
+              <Alert key="error" variant="danger">
+                Failed to fetch songs: {error}
+              </Alert>
+            )
+            : songs.map(item => (
+              <li>{item}</li>
+            ))
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
